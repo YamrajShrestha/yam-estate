73bcea86a5226b84a99587055eaffb23971788ae
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-03-03 11:39:08 +0545

    created a test Api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,10 +16,8 @@ const app = express();
 
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use("/api/user", userRouter);
