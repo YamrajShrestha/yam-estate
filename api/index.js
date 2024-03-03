46a69ec7f09c6e86fb3d89570e85587b44d4d375
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-03-03 14:12:30 +0545

    Created a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -14,13 +14,22 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
-
 const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
